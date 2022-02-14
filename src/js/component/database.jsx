c7e546843ef0ebfe3ca7ee88b8b42676e63fb86d
Jose <josejesuscedeno@gmail.com>
2022-02-14 22:06:12 +0000

    tasklist

--- src/js/component/database.jsx
+++ src/js/component/database.jsx
@@ -3,11 +3,15 @@ import React from "react";
 const Database = () => {
 	const tasklist = [{ name: "comer" }, { name: "vivir" }];
 
+	const handleChange = (event) => {
+		console.log(arr.push(event.target.value));
+	};
+
 	return (
 		<div>
 			<ul>
 				{tasklist.map((value, index) => {
-					return <li>{value.name}</li>;
+					return <li key={index}></li>;
 				})}
 			</ul>
 		</div>
--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -1,7 +1,18 @@
+import { arrayOf } from "prop-types";
 import React, { useState } from "react";
-
+//notas: para agregar un nuevo item a un array es arr.push()
 const InputTask = () => {
 	const [text, setText] = useState("");
+
+	const [tasklist, setTasklist] = useState([
+		{ name: "comer" },
+		{ name: "vivir" },
+		{ name: "dormir" },
+	]);
+
+	const handleClick = (event) => {
+		console.log(event.target.value);
+	};
 
 	const handleChange = (event) => {
 		console.log(event.target.value);
@@ -12,7 +23,19 @@ const InputTask = () => {
 		<div>
 			<h3>{text}</h3>
 			<input type="text" name="search" onChange={handleChange} />
-			<button>Create</button>
+			<button onClick={handleClick}>Create</button>
+			<p></p>
+			<p></p>
+			<p></p>
+			<ul>
+				{tasklist.map((value, index) => {
+					return (
+						<li key={index}>
+							{value.name} index: {index}
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 };
